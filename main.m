b299bb57b69517d2aea2e162909a42f586d5d70b
Antonio <antonio.di.mauro.23@gmail.com>
2021-11-16 18:20:28 +0100

    added SGD

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear variables;
 
-% load datasets
+%% load datasets
 monks1_train_filename = 'datasets/monks/monks-1.train';
 monks2_train_filename = 'datasets/monks/monks-2.train';
 monks3_train_filename = 'datasets/monks/monks-3.train';
@@ -36,13 +36,17 @@ monks3_y_test = monks3_test(:,7);
 cup_train = readtable(cup_filename,'FileType','text');
 cup_x_train = cup_train(1:1300,1:20);
 cup_y_train = cup_train(1:1300,21:22);
+
 cup_x_test = cup_train(1301:end,1:20);
 cup_y_test = cup_train(1301:end,21:22);
 
+%% the problem
+A = [2 5;1 7];
+b = [100 70]';
 
+x0 = [-37,88]';
+t = 0.1; % momentum parameter
+eps = 1e-10;
+MaxIter = 1000;
 
-
-
-
-
-
+[x] = SGD(A, b, x0, eps, MaxIter);
