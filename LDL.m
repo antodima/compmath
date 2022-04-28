593fe3239ac71b00518eb96742eb3f493299a605
Antonio <antonio.di.mauro.23@gmail.com>
2022-04-28 13:45:56 +0300

    updated pivoting

--- LDL.m
+++ LDL.m
@@ -2,14 +2,29 @@ function [L, D, P, x] = LDL(A, b, pivoting)
 % LDL factorization
     m = size(A, 1);
     L = eye(m); D = zeros(m);
-    P = 1:m;
+    P = eye(m);
+    A_diag = diag(A);
     for k = 1:m-1
         % pivoting
         if pivoting
-            [unused, p] = max(abs(A(k:end, k)));
-            p = k-1 + p;
-            A([k,p], 1:end) = A([p,k], 1:end);
-            P([k, p]) = P([p, k]);
+            %[unused, p] = max(abs(A(k:end, k)));
+            %p = k-1 + p;
+            %P([k,p],:) = P([p,k],:);
+            %A=P*A;
+            
+            % https://github.com/hsulab/MatrixFactorization/blob/70da3743eada50506d0a1c3b63274cdb09b1d7f1/LDL.m
+            pivot = max(abs(A_diag(k:m)));
+            for j=k:m
+                if abs(A_diag(j)) == pivot
+                    ind = j;
+                end
+            end
+
+            if j ~= k
+                P(:,[k,ind]) = P(:,[ind,k]);
+                A([k,ind],:) = A([ind,k],:);
+                A(:,[k,ind]) = A(:,[ind,k]);
+            end
         end
         
         D(k, k) = A(k, k);
@@ -17,8 +32,5 @@ function [L, D, P, x] = LDL(A, b, pivoting)
         A(k+1:end, k+1:end) = A(k+1:end, k+1:end) - L(k+1:end, k) * A(k, k+1:end);
     end
     D(m, m) = A(m, m);
-    if pivoting
-        L = L(P,:);
-    end
-    x = L' \ ((L\b) ./ diag(D)); % solve the linear system
+    x = L'*P' \ ((P*L\b) ./ diag(D)); % solve the linear system
 end
